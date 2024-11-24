c86ec1fd3c74bb9f1ea1fd6708c96cbcd31607b8
Rohan Sarkar <rohansa@backbase.com>
2024-11-24 22:54:37 +0530

    Duplication fix

--- app/src/main/java/com/rohansarkar/frameify/db/face_detection/FaceDetection.kt
+++ app/src/main/java/com/rohansarkar/frameify/db/face_detection/FaceDetection.kt
@@ -2,11 +2,12 @@ package com.rohansarkar.frameify.db.face_detection
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "face_detection")
+@Entity(tableName = "face_detection", indices = [Index(value = ["image_uri"], unique = true)])
 data class FaceDetection(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo(name = "image_uri", index = true) val imageUri: String,
+    @ColumnInfo(name = "image_uri") val imageUri: String,
     @ColumnInfo(name = "contains_face") val containsFace: Boolean? = null // Null if detection not done
 )
